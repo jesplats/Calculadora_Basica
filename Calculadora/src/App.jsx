9357e554d154c6d8ef1543ce62cec74f19d8a9ce
jesus_plata <jesusplata1520@gmail.com>
2025-04-09 10:48:36 -0500

    a

--- Calculadora/src/App.jsx
+++ Calculadora/src/App.jsx
@@ -7,7 +7,7 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
+    <main>
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -21,14 +21,10 @@ function App() {
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+     
+    </main>
   )
 }
 
